dea1e79c7790a67d0847016cccbd2048c3562112
ikkyu <ikkyu_zx@163.com>
2020-05-05 14:15:08 +0800

    增加redis测试类

---
+++ sample-test/src/test/java/cn/ikkyu/sample/test/webservice/RedisTest.java
@@ -0,0 +1,11 @@
+package cn.ikkyu.sample.test.webservice;
+
+import org.junit.runner.RunWith;
+
+/**
+ * @author xinming
+ * @Date 2020/5/5 14:10
+ */
+@RunWith(SpringRunner.class)
+public class RedisTest {
+}
